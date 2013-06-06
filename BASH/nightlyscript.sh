f0b3aa75cf8ee37be13daa95e8bc9a57f61d238a
Luke Bilston <lbilston@gmail.com>
2013-06-07 09:00:07 +1000

    Folder Update / included script

---
+++ BASH/nightlyscript.sh
@@ -0,0 +1,2 @@
+php /path/to/nagiosconfig/index.php > /usr/local/nagios/etc/configs/all.cfg
+/etc/init.d/nagios reload
--- SQL/nagiosconfig.sql
+++ SQL/nagiosconfig.sql
@@ -1,30 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 3.4.11.1deb1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: May 24, 2013 at 08:55 PM
--- Server version: 5.5.31
--- PHP Version: 5.4.6-1ubuntu1.2
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `nagiosconfig`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `devices`
---
 
 CREATE TABLE IF NOT EXISTS `devices` (
   `IP` varchar(13) DEFAULT NULL,
@@ -34,11 +7,7 @@ CREATE TABLE IF NOT EXISTS `devices` (
   `Sort` int(3) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `hostGroups`
---
 
 CREATE TABLE IF NOT EXISTS `hostGroups` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -47,6 +16,3 @@ CREATE TABLE IF NOT EXISTS `hostGroups` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
